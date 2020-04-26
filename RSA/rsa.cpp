f23a134ce7691dd64c74bf07a446939eb7c78d35
Noc0r <senatrevr@mail.ru>
2020-04-26 20:39:47 +0300

    change decrypt RSA

--- RSA/rsa.cpp
+++ RSA/rsa.cpp
@@ -12,6 +12,9 @@ RSA::RSA(int l, bool (*pvmt_func)(int, BigInteger, int))
         q = genPrime(q_size, pvmt_func);
     } while (extendEucl(p - 1, params.e).d != 1 || extendEucl(q - 1, params.e).d != 1);
     params.N = p * q;
+    eucl_res inv = extendEucl(p, q);
+    p_inv = inv.x;
+    q_inv = inv.y;
     BigInteger phi = (p - 1) * (q - 1);
     params.d = extendEucl(params.e, phi).x;
 }
@@ -23,5 +26,7 @@ BigInteger RSA::encrypt(BigInteger x)
 
 BigInteger RSA::decrypt(BigInteger y)
 {
-    return modPow(y, params.d, params.N);
+    BigInteger x1 = modPow(y, params.d % (p - 1), p);
+    BigInteger x2 = modPow(y, params.d % (q - 1), q);
+    return (q_inv * q * x1 + p_inv * p * x2) % params.N;
 }
